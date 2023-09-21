cc9d475acda3790d37ebe2e6b6e231131d209613
Edahí Antonio Gutiérrez Reyes <edahigure@gmail.com>
2023-09-20 19:51:57 -0600

    Make basic setup rails react and redux

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
+  get 'root/index'
+  resources :mesages
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  namespace :api do
+    namespace :v1 do
+      resources :messages
+    end
+  end
+
+  root 'root#index'
 end
---
+++ test/controllers/mesages_controller_test.rb
@@ -0,0 +1,48 @@
+require "test_helper"
+
+class MesagesControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @mesage = mesages(:one)
+  end
+
+  test "should get index" do
+    get mesages_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get new_mesage_url
+    assert_response :success
+  end
+
+  test "should create mesage" do
+    assert_difference("Mesage.count") do
+      post mesages_url, params: { mesage: { text: @mesage.text } }
+    end
+
+    assert_redirected_to mesage_url(Mesage.last)
+  end
+
+  test "should show mesage" do
+    get mesage_url(@mesage)
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get edit_mesage_url(@mesage)
+    assert_response :success
+  end
+
+  test "should update mesage" do
+    patch mesage_url(@mesage), params: { mesage: { text: @mesage.text } }
+    assert_redirected_to mesage_url(@mesage)
+  end
+
+  test "should destroy mesage" do
+    assert_difference("Mesage.count", -1) do
+      delete mesage_url(@mesage)
+    end
+
+    assert_redirected_to mesages_url
+  end
+end
