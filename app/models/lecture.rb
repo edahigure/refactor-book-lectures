7458853d084c415b0ac8ec7266cea0b3d443357a
Edahí Antonio Gutiérrez Reyes <edahigure@gmail.com>
2023-10-08 09:42:18 -0600

    Add associations to models

--- app/models/lecture.rb
+++ app/models/lecture.rb
@@ -1,3 +1,4 @@
 class Lecture < ApplicationRecord
   belongs_to :teacher
+  has_many :reservations
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
+  has_many :reservations
+  has_many :inscriptions
 
          
 end
